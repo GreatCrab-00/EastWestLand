be7d81b01be22101d1789d547e3e4bc75f1c4a5c
GreatCrab-00 <93721038+GreatCrab-00@users.noreply.github.com>
2022-01-16 14:17:53 -0600

    Some GroundWork

--- EastWestLand/src/LandPackage/Game.java
+++ EastWestLand/src/LandPackage/Game.java
@@ -3,12 +3,78 @@ package LandPackage;
  * Gabe E., Aiden T., David S., and Sudeep S.
  * This program will hold all of the games code.
  */
+
+
+
+import java.util.*;
+import java.lang.Math;
 public class Game {
-	//properties
-	int phealth;//other stats listed here
+	//general properties
+	Scanner reader;
+	
+	
+	
+	//player properties
+	private int pHealth,pHMax;//other player stats listed here
+	private int[] itemBag;
+	
+	
+	
+	//Boss Properties
+	private int bHealth,bossNum;// other Boss related stats.
+	
+	
+	//constructor
+	public Game() {
+		//general
+		reader = new Scanner(System.in);
+		
+		
+		
+		//player
+		pHMax = 50;
+		pHealth = pHMax;
+		
+		
+		itemBag = new int[4];
+		for(int i = 0; i < 4;i++)
+			itemBag[i] = 0;//0 will stand for an empty slot.
+		
+		//boss
+		bossNum = 1;
+		bHealth = bossNum * 25;
+		
+	}
+	//General Behaviors
+	public int Input() {//this behavior is here to try and catch errors.
+		int input;
+		try {
+			input = reader.nextInt();
+			
+		}catch(Exception e) {
+			input = -1;
+		}
+		return input;
+	}
+	
+	
+	
+	//player Behaviors
+	
+	
+	
+	
+	//Boss Behaviors
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 }
