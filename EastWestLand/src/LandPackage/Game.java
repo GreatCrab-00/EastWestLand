a7861a55633dcd1ce3744017699bb48b1f3ba781
GreatCrab-00 <93721038+GreatCrab-00@users.noreply.github.com>
2022-01-16 13:49:46 -0600

    Game class

---
+++ EastWestLand/src/LandPackage/Game.java
@@ -0,0 +1,14 @@
+package LandPackage;
+/* 1/16/22
+ * Gabe E., Aiden T., David S., and Sudeep S.
+ * This program will hold all of the games code.
+ */
+public class Game {
+	//properties
+	int phealth;//other stats listed here
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
