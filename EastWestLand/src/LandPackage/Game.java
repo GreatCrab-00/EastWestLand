73a0e18229da7134380b626828d211e46bedf57a
GreatCrab-00 <93721038+GreatCrab-00@users.noreply.github.com>
2022-01-17 12:24:45 -0600

    Boss flavor text

--- EastWestLand/src/LandPackage/Game.java
+++ EastWestLand/src/LandPackage/Game.java
@@ -28,6 +28,9 @@ public class Game {
 	 * p - piercing damage
 	 * r - random damage
 	 */
+	
+	
+	
 	//constructor
 	public Game() {
 		//general
@@ -79,12 +82,54 @@ public class Game {
 	
 	//Boss Behaviors
 	public void BossAttack() {
-		
+		switch(bossNum) {
+		case 1:
+			 System.out.println("The " + BossName() + " Proposes a series of intricate and dense series of formulas and theories that you can't quite comprehend.");//
+			break;
+		case 2:
+			System.out.println("The " + BossName() + " Raises a few skeletons from the ground who then throw their ribcages at you before falling apart.");//
+			break;
+		case 3:
+			System.out.println("The " + BossName() + " Slithers through some thorn bushes and you take the most direct path to catch it.");//
+			break;
+		case 4:
+			System.out.println("The " + BossName() + " summons all words that include \"T\" to attack you.");//
+			break;
+		case 5:
+			System.out.println("The " + BossName() + " Raises his sword to reflect the light of the eclipse directly back into your eyes.");//
+			break;
+		default:
+			System.out.println("The " + BossName() + " Starts breaking the world around you.");//
+			break;
+		}
 	}
 	public void BossSetUp() {
 		
 	}
-	
+	public String BossName() {
+		String name = "";
+		switch(bossNum) {
+		case 1:
+			name = "Rombus";
+			break;
+		case 2:
+			name = "Bone Mage";
+			break;
+		case 3:
+			name = "Garden Snake";
+			break;
+		case 4:
+			name = "Letter T";
+			break;
+		case 5:
+			name = "Knight of the Eclipse";
+			break;
+		default:
+			name = "Error";
+			break;
+		}
+		return name;
+	}
 	
 	
 	
