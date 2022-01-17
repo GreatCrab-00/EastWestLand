f60356c6545d4f7b6319f63214d42ec25401c18a
GreatCrab-00 <93721038+GreatCrab-00@users.noreply.github.com>
2022-01-17 17:55:56 -0600

    some Progress

--- EastWestLand/src/LandPackage/Game.java
+++ EastWestLand/src/LandPackage/Game.java
@@ -15,9 +15,9 @@ public class Game {
 	
 	
 	//player properties
-	private int pHealth,pHMax;//other player stats listed here
+	private int pHealth,str,luck,con;//other player stats listed here
 	private int[] itemBag;
-	
+	private boolean dodge;
 	
 	
 	//Boss Properties
@@ -39,9 +39,11 @@ public class Game {
 		
 		
 		//player
-		pHMax = 50;
-		pHealth = pHMax;
-		
+		str = 1;
+		luck = 1;
+		con = 1;
+		pHealth = 30 + 20 * con;
+		dodge = false;
 		
 		itemBag = new int[4];
 		for(int i = 0; i < 4;i++)
@@ -62,7 +64,9 @@ public class Game {
 		}
 		return input;
 	}
-	
+	private int MRand(int min,int max) {
+		return (int)((double)(max - min)*Math.random() + (double)(min)+0.5);
+	}
 	
 	
 	//player Behaviors
@@ -73,7 +77,26 @@ public class Game {
 		
 	}
 	public void PlayerCheck() {
-		
+		switch(bossNum) {
+		case 1:
+			 System.out.println("The " + BossName() + " looks very brittle and probably doesn't want probibility anywhere near it's mathematical concepts.");//Mathematical
+			break;
+		case 2:
+			System.out.println("The " + BossName() + " is actually a Skeleton in a robe and has a heart in the middle of it's ribcage.");//
+			break;
+		case 3:
+			System.out.println("The " + BossName() + " it is too quick for your hammer, but You can still spear it or slice it pretty easly.");//
+			break;
+		case 4:
+			System.out.println("The " + BossName() + " You think you could easily slice it in half.");//
+			break;
+		case 5:
+			System.out.println("The " + BossName() + " has weak points in their armor at the joints. You can also easily dent his armor.");//
+			break;
+		default:
+			System.out.println("The " + BossName() + " has no weekness.");//
+			break;
+		}
 	}
 	public void PlayerDodge() {
 		
@@ -82,29 +105,101 @@ public class Game {
 	
 	//Boss Behaviors
 	public void BossAttack() {
-		switch(bossNum) {
+		if(!dodge) {
+			switch(bossNum) {
+			case 1:
+				System.out.println("The " + BossName() + " proposes a series of intricate and dense series of formulas and theories that you can't quite comprehend.");//
+				break;
+			case 2:
+				System.out.println("The " + BossName() + " raises a few skeletons from the ground who then throw their ribcages at you before falling apart.");//
+				break;
+			case 3:
+				System.out.println("The " + BossName() + " slithers through some thorn bushes and you take the most direct path to catch it.");//
+				break;
+			case 4:
+				System.out.println("The " + BossName() + " summons all words that include \"T\" to attack you.");//
+				break;
+			case 5:
+				System.out.println("The " + BossName() + " raises his sword to reflect the light of the eclipse directly back into your eyes.");//
+				break;
+			default:
+				System.out.println("The " + BossName() + " Starts breaking the world around you.");//
+				break;
+			}
+		}
+		else
+			System.out.println("You roll to the left and dodge their attack.");
+		dodge = false;
+	}
+	public void BossSetUp() {
+		System.out.print("You have Successfully killed the " + BossName() + ".\nPlease choose a stat to level up.\n(1)Strength : " + str + "\n(2)Constitution : " + con + "\n(3)Luck : " + luck + "\n>>");//Constitution
+		int choice = 0;
+		choice = Input();
+		if(choice > 3|| choice > 1)
+			choice = MRand(1,3);
+		switch(choice) {
 		case 1:
-			 System.out.println("The " + BossName() + " Proposes a series of intricate and dense series of formulas and theories that you can't quite comprehend.");//
+			str++;
+			System.out.println("Strength increased to " + str + ".");
 			break;
 		case 2:
-			System.out.println("The " + BossName() + " Raises a few skeletons from the ground who then throw their ribcages at you before falling apart.");//
+			con++;
+			System.out.println("Constitution increased to " + con + ".");
 			break;
 		case 3:
-			System.out.println("The " + BossName() + " Slithers through some thorn bushes and you take the most direct path to catch it.");//
-			break;
-		case 4:
-			System.out.println("The " + BossName() + " summons all words that include \"T\" to attack you.");//
-			break;
-		case 5:
-			System.out.println("The " + BossName() + " Raises his sword to reflect the light of the eclipse directly back into your eyes.");//
+			luck++;
+			System.out.println("Luck increased to " + luck + ".");
 			break;
 		default:
-			System.out.println("The " + BossName() + " Starts breaking the world around you.");//
+			System.out.println("Somehow you get no stats.");
+			break;
+		}
+		bossNum++;
+		System.out.println("You suddenly encounter an even more deadlier foe than the "+BossName(1)+", the " +BossName()+".");
+		switch(bossNum) {
+		case 1:
+			bDamVul = "br";
+			break;
+		case 2:
+			bDamVul = "bp";
+			break;
+		case 3:
+			bDamVul = "sp";
+			break;
+		case 4:
+			bDamVul = "s";
+			break;
+		case 5:
+			bDamVul = "pb";
+			break;
+		default:
+			bDamVul = "";
 			break;
 		}
 	}
-	public void BossSetUp() {
-		
+	public String BossName(int offset) {//this overloaded behavior will display the current boss's name unless a number is put into the parameter which offsets the current boss to refer to previous bosses.
+		String name = "";
+		switch(bossNum-offset) {
+		case 1:
+			name = "Rombus";
+			break;
+		case 2:
+			name = "Bone Mage";
+			break;
+		case 3:
+			name = "Garden Snake";
+			break;
+		case 4:
+			name = "Letter T";
+			break;
+		case 5:
+			name = "Knight of the Eclipse";
+			break;
+		default:
+			name = "Error";
+			break;
+		}
+		return name;
 	}
 	public String BossName() {
 		String name = "";
